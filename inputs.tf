a8ae4666ec9eb4e033409edf7db20a43a28efe68
Immersive <105320165+immmersive@users.noreply.github.com>
2024-09-26 23:18:43 +0900

    Create inputs.tf

---
+++ inputs.tf
@@ -0,0 +1,19 @@
+variable "app_name" {    
+    default     = ""
+}
+ 
+variable "region" {
+    default     = ""
+}
+
+variable "availability_zone" {
+    default     = ""
+}
+
+variable "blueprint_id" {    
+    default     = ""
+}
+ 
+variable "bundle_id" {
+    default     = ""
+}
