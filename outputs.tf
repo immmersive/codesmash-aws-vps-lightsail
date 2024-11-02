406ebd24bcbe86621e4e9b8689c65bebb934a5f1
Immersive <105320165+immmersive@users.noreply.github.com>
2024-11-03 02:36:25 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "instance_name" {
   value = aws_lightsail_instance.lightsail.name
 }
+
+output "lightsail_static_ip" {  
+  value         = var.has_static_ip == "true" ? aws_lightsail_static_ip.static_ip[0].ip_address : null
+}
