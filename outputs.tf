726e04a0265a1a6a87c2ab166f97cc3773d0b04e
Immersive <105320165+immmersive@users.noreply.github.com>
2024-11-11 23:11:04 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "instance_name" {
 output "lightsail_static_ip" {  
   value         = var.has_static_ip == "true" ? aws_lightsail_static_ip.static_ip[0].ip_address : null
 }
+
+output "load_balancer_dns" {
+  value         = length(var.lb_instances) > 0 ? aws_lightsail_lb.load_balancer[0].dns_name : null
+}
+
+output "lb_instances" {
+  value         = var.lb_instances
+}
