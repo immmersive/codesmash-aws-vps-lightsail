95ad091062f30ee87982d9f91aadca41131cff7b
Immersive <105320165+immmersive@users.noreply.github.com>
2024-11-13 23:42:02 +0900

    Update record.tf

--- record.tf
+++ record.tf
@@ -1,10 +1,10 @@
 resource "aws_route53_zone" "domain" {
-    count   = var.domain != null ? 1 : 0
+    count   = var.domain != "" ? 1 : 0
     name    = var.domain
 }
  
 resource "aws_route53_record" "distribution_record" {
-    for_each    = var.domain != null && length(var.subdomains) > 0 ? toset(var.subdomains) : toset([])
+    for_each    = var.domain != "" && length(var.subdomains) > 0 ? toset(var.subdomains) : toset([])
     zone_id     = aws_route53_zone.domain[0].zone_id
     name        = "${each.value}.${var.domain}"
     type        = "CNAME"
