0025bdee9ab67ab4a0c4a6cc643d1d1277a94160
Immersive <105320165+immmersive@users.noreply.github.com>
2024-11-13 09:40:49 +0900

    Update inputs.tf

--- inputs.tf
+++ inputs.tf
@@ -26,3 +26,15 @@ variable "lb_instances" {
     type =      list(string)
     default     = []
 }
+
+variable "has_distribution" {
+    default     = "true"
+}
+
+variable "distribution_size" {
+    default     = "small_1_0"
+}
+
+variable "distribution_cache_behavior" {
+    default     = "dont-cache"
+}
