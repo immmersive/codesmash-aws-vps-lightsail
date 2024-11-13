17060406359be458bf5c26242456d5c0f16736d3
Immersive <105320165+immmersive@users.noreply.github.com>
2024-11-13 23:39:21 +0900

    Create record.tf

---
+++ record.tf
@@ -0,0 +1,13 @@
+resource "aws_route53_zone" "domain" {
+    count   = var.domain != null ? 1 : 0
+    name    = var.domain
+}
+ 
+resource "aws_route53_record" "distribution_record" {
+    for_each    = var.domain != null && length(var.subdomains) > 0 ? toset(var.subdomains) : toset([])
+    zone_id     = aws_route53_zone.domain[0].zone_id
+    name        = "${each.value}.${var.domain}"
+    type        = "CNAME"
+    ttl         = 300
+    records     = [aws_lightsail_distribution.distribution[0].domain_name]
+}  
