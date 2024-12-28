ab98f2dd620b3a8efc1833c95d416fd6cb0f3e27
Immersive <105320165+immmersive@users.noreply.github.com>
2024-12-28 21:43:32 +0900

    Update lightsail.tf

--- lightsail.tf
+++ lightsail.tf
@@ -2,6 +2,9 @@ resource "aws_lightsail_instance" "lightsail" {
     name                = "${var.app_name}_${terraform.workspace}"
     availability_zone   = "${var.availability_zone}" 
     blueprint_id        = "${var.blueprint_id}" 
-    bundle_id           = "${var.bundle_id}" 
-    user_data           = file("user_data.sh")
+    bundle_id           = "${var.bundle_id}"  
+    user_data = <<EOT
+${var.selected_app != "" ? file(var.selected_app) : ""}
+${file("user_data.sh")}
+EOT
 }
